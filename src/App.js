50a38a4df5f91a796b69caa48f1d225cf1bf077c
Nicholas Batschynsky <nicholasbatschynsky@Nicholass-MacBook-Air.local>
2023-03-21 01:48:14 -0400

    Victory

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload -- I am 
+          experiencing an immense wave of victory and relief after 
+          figuring out why my React wouldn't sync with the Github repository..
         </p>
         <a
           className="App-link"
@@ -15,9 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Nick, who is experiecing an immense wave
-          of victory and relief after figuring out why his React wouldnt sync 
-          with his Github repository
+          Learn React with Nick and MSU!
         </a>
       </header>
     </div>
